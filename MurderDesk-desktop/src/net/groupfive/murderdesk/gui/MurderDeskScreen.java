5f50aed54b7845c90837d0d5e04756e5ba314b43
TeisD <teis.degreve@gmail.com>
2014-01-04 15:50:15 +0100

    initial commit (test)

---
+++ MurderDesk-desktop/src/net/groupfive/murderdesk/gui/MurderDeskScreen.java
@@ -0,0 +1,12 @@
+package net.groupfive.murderdesk.gui;
+
+import javax.swing.JFrame;
+
+public class MurderDeskScreen extends JFrame{
+	
+	public MurderDeskScreen(String title){
+		super(title);
+		setSize(800,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
