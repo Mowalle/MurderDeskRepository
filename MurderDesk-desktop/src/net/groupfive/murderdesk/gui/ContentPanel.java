df1575ec0de39f4bdde0bf980dc77b67d427b1dd
TeisD <teis.degreve@gmail.com>
2014-01-06 23:56:35 +0100

    now uses the correct font

--- MurderDesk-desktop/src/net/groupfive/murderdesk/gui/ContentPanel.java
+++ MurderDesk-desktop/src/net/groupfive/murderdesk/gui/ContentPanel.java
@@ -1,12 +1,14 @@
 package net.groupfive.murderdesk.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
+
+import net.groupfive.murderdesk.Main;
 
 @SuppressWarnings("serial")
 public class ContentPanel extends JPanel{
@@ -14,15 +16,15 @@ public class ContentPanel extends JPanel{
 	public ContentPanel(String t){
 		setOpaque(false);
 		setSize(100,100);
+		setLayout(new BorderLayout());
 		
 		// some defaults to reuse
 		Border whiteBorder = BorderFactory.createCompoundBorder(
 				BorderFactory.createEmptyBorder(0,1,0,1),
 				BorderFactory.createLineBorder(Color.WHITE, 2));
-		Font titleFont = new Font("Camcorder", Font.PLAIN, 16);
 		
 		// add border
-		TitledBorder title = BorderFactory.createTitledBorder(whiteBorder, t, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, titleFont, Color.WHITE);
+		TitledBorder title = BorderFactory.createTitledBorder(whiteBorder, t, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, Main.ftTitle2, Color.WHITE);
 		setBorder(title);
 	}
 }
