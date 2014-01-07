e00e86e93236c3d3a110cde79746c71b6939240f
TeisD <teis.degreve@gmail.com>
2014-01-07 16:33:17 +0100

    Organized startup function

--- MurderDesk-desktop/src/net/groupfive/murderdesk/Main.java
+++ MurderDesk-desktop/src/net/groupfive/murderdesk/Main.java
@@ -1,29 +1,51 @@
 package net.groupfive.murderdesk;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.ScrollPane;
 import java.io.InputStream;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+import javax.swing.text.StyledDocument;
 
 import net.groupfive.murderdesk.gui.GamePanel;
 import net.groupfive.murderdesk.gui.ContentPanel;
 import net.groupfive.murderdesk.gui.MurderDeskScreen;
+import net.groupfive.murderdesk.gui.NormalText;
+import net.groupfive.murderdesk.gui.NormalTextScroll;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
 
 public class Main {
+	
+	public final static boolean GUI = true;
 	
 	public static Font ftCamcorder, ftMinecraftia, ftTitle1, ftTitle2, ftRegular, ftSmall;
 	
 	/**
-	 * Start the game, either just the game in a single window, or as part of the full GUI.
-	 * Comment or uncomment as needed. Note that executing both functions will throw a NullPointerException
+	 * Creates and shows the game or user interface.
+	 * Use the GUI boolean to enable or disable the three-screen UI.
 	 */
 	public static void main(String[] args) {
 		
-		//runAsSingleWindow();
-		
-		initGUI();
-        
+		if(GUI){
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				@Override
+	            public void run() {
+	                createAndShowGUI();
+	            }
+	        });
+		} else{
+			runAsSingleWindow();
+		}
 	}
 	
 	public static void runAsSingleWindow(){
@@ -39,7 +61,7 @@ public class Main {
 	/**
 	 * Very long and messy coded function that sets up the GUI
 	 */
-	public static void initGUI(){
+	public static void createAndShowGUI(){
 		// add fonts globally
 		try {
 			InputStream s1 = Main.class.getResourceAsStream("/fonts/CAMCORDER_REG.otf");
@@ -79,15 +101,21 @@ public class Main {
 	}
 	
 	private static void initScreen2(final MurderDeskScreen s){
-		GamePanel pCamera = new GamePanel("Camera");
+		ContentPanel pCamera = new ContentPanel("Camera");
 		ContentPanel pSound = new ContentPanel("Sound");
 		ContentPanel pTraps = new ContentPanel("Traps");
 		ContentPanel pConsole = new ContentPanel("Console");
 		
-		pCamera.setBounds(0, 0, 640, 400);
+		//pCamera.setBounds(0, 0, 640, 400);
+		pCamera.setBounds(0,0,640,400);
 		pSound.setBounds(640, 0, 140, 400);
 		pTraps.setBounds(0, 400, 300, 156);
 		pConsole.setBounds(300, 400, 480, 156);
+		
+		//initialize & add the individual components
+		NormalText txtConsole = new NormalText();
+		NormalTextScroll txtConsoleScroll = new NormalTextScroll(txtConsole);
+		pConsole.add(txtConsoleScroll, BorderLayout.CENTER);
 		
 		s.content.add(pCamera);
 		s.content.add(pSound);
@@ -103,6 +131,10 @@ public class Main {
 		pSubject.setBounds(0, 0, 500, 556);
 		pObjectives.setBounds(500, 0, 280, 400);
 		pBalance.setBounds(500, 400, 280, 156);
+		
+		//initialize & add the individual components
+		NormalText txtObjectives = new NormalText();
+		pObjectives.add(txtObjectives, BorderLayout.CENTER);
 		
 		s.content.add(pSubject);
 		s.content.add(pObjectives);
